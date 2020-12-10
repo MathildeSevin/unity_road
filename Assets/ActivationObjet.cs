61375688345b91b33f3e70b4280febae9ac6d075
MathildeSevin <75262542+MathildeSevin@users.noreply.github.com>
2020-12-11 00:30:13 +0100

    Add files
    Add main scene

---
+++ Assets/ActivationObjet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationObjet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public GameObject objetFixe;
+    public GameObject objetMobile;
+    public GameObject objetMixte;
+
+    void Start()
+    {
+        objetFixe.SetActive(false);
+        objetMobile.SetActive(false);
+        objetMixte.SetActive(false);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        objetFixe.SetActive(true);
+        objetMobile.SetActive(true);
+        objetMixte.SetActive(true);
+    }
+}
---
+++ Assets/TransformTranslate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformTranslate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 5f);
+    }
+
+}
